199680fe93387e7b1bae326a2750f780d6dc94be
Samantha Wong <smiwong920@gmail.com>
2019-02-28 20:04:57 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,31 +1,31 @@
 class Book
  
   def initialize(title)
-    @title = title
+    @title=title
   end
  
   def title
     @title
   end
   
-  def author = (author)
-    @author = author
+  def author=(author)
+    @author=author
   end
   
   def author
     @author
   end 
   
-  def page_count = (num)
-    @page_count = num
+  def page_count=(num)
+    @page_count=num
   end 
   
   def page_count
     @page_count
   end 
   
-  def genre = (genre)
-    @genre = genre
+  def genre=(genre)
+    @genre=genre
   end 
   
   def genre
